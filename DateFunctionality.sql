3cd732c4c89f8b818cd067b89c5844f186f8bb4c
mr-indonesia <66452890+mr-indonesia@users.noreply.github.com>
2025-08-27 16:56:25 +0700

    Update DateFunctionality.sql

--- DateFunctionality.sql
+++ DateFunctionality.sql
@@ -9,4 +9,22 @@ SELECT dbo.UFN_LEVENSHTEIN	(
 --FUNCTION RETURN DATE
 /*----------------------------------------------------------------------------------------*/
 SELECT convert(varchar(20),GETDATE(),106) as Result
+
 /*----------------------------------------------------------------------------------------*/
+
+git config --global user.name "Johan Koesnadi"
+git config --global user.email "johan.koesnadi@mitra.takaful.com"
+
+# Clone tanpa checkout
+git clone --no-checkout https://git.takaful.com/atk/quotation-non-health.git quotation-non-health
+cd quotation-non-health
+
+# Aktifkan sparse checkout
+git config core.sparseCheckout true
+
+# Tentukan folder yang diinginkan
+echo "min-quotation-takaful-apps/" >> .git/info/sparse-checkout
+echo "ux-quotation-takaful-apps/" >> .git/info/sparse-checkout
+
+# Lakukan checkout
+git checkout main
