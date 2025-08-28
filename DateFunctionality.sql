2025350fba9381b18a6634bb91b332b55f46bfda
mr-indonesia <66452890+mr-indonesia@users.noreply.github.com>
2025-08-29 03:08:49 +0700

    Update DateFunctionality.sql

--- DateFunctionality.sql
+++ DateFunctionality.sql
@@ -12,6 +12,39 @@ SELECT convert(varchar(20),GETDATE(),106) as Result
 
 /*----------------------------------------------------------------------------------------*/
 
+----------------------------------------------------------------------------
+git config --global user.name mr-indonesia
+git config --global user.email secretcode190@gmail.com
+
+git init
+git add .
+git commit -m "initial commit"
+
+git remote add origin https://github.com/mr-indonesia/dbsbankpostingengine.git
+git remote -v
+
+git branch -M main
+git push -u origin main
+
+git checkout -b NayyaraBranch
+git branch NayyaraBranch
+git push -u origin NayyaraBranch
+git branch -a
+git branch *
+git checkout NayyaraBranch
+----------------------------------------------------------------------------
+git config --global user.name "Johan Koesnadi"
+git config --global user.email "johan.koesnadi@mitra.takaful.com"
+
+git remote add origin git@git.takaful.com:atk/quotation-non-health.git
+git remote -v
+
+git pull origin master
+git pull origin uat
+
+git checkout uat
+----------------------------------------------------------------------------
+
 git config --global user.name "Johan Koesnadi"
 git config --global user.email "johan.koesnadi@mitra.takaful.com"
 
@@ -28,3 +61,4 @@ echo "ux-quotation-takaful-apps/" >> .git/info/sparse-checkout
 
 # Lakukan checkout
 git checkout main
+
